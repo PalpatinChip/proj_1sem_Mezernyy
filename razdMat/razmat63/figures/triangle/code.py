a30068a12da0fc35d74bcd9011b2fdc44515f615
PalpatinChip <80686755+PalpatinChip@users.noreply.github.com>
2022-03-23 17:44:52 +0300

    Update code.py

--- razdMat/razmat63/figures/triangle/code.py
+++ razdMat/razmat63/figures/triangle/code.py
@@ -1,12 +1,12 @@
 import math
 
-a, b, c = 7, 2, 8
+_a, _b, _c = 7, 2, 8
 
 
-def triangle_perimetr(aa=a, bb=b, cc=c):
+def triangle_perimetr(aa=_a, bb=_b, cc=_c):
     return aa + bb + cc
 
 
-def triangle_area(aa=a, bb=b, cc=c):
+def triangle_area(aa=a, bb=_b, cc=_c):
     p = (aa + bb + cc)/2
     return math.sqrt(p*float(p-aa)*float(p-bb)*float(p-cc))
